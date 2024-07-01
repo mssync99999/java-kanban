ffb2d96433401e3d08c38ec5637f445a46c08a05
Boris <mssync99999@gmail.com>
2024-07-01 11:34:10 +0300

    Sprint 6. Change method of History. 13st time

--- src/controllers/HistoryManager.java
+++ src/controllers/HistoryManager.java
@@ -5,8 +5,9 @@ import tickets.Task;
 import java.util.List;
 
 public interface HistoryManager {
-
     void add(Task task);
+
     List<Task> getHistory();
+
     void remove(int id); //sprint 6
 }
