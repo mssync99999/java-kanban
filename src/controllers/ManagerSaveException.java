a06993de1d14e3a308c261a55c1cbe63f2cb8930
Boris <mssync99999@gmail.com>
2024-07-20 20:45:22 +0300

    Sprint 7. 002

--- src/controllers/ManagerSaveException.java
+++ src/controllers/ManagerSaveException.java
@@ -1,7 +1,5 @@
 package controllers;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message) {
         super(message);
